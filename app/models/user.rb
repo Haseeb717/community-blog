dac6d275a3aa72999f468a7f82a33f9e7ebcd99f
phaedra <haseeb.ahmad717@gmail.com>
2015-09-18 11:07:41 +0500

    user and comments association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
 	# :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable, :registerable,:recoverable, :rememberable, :trackable, :validatable
 
-	has_many:products
+	has_many :products
   has_many :user_tags
   has_many :tags, through: :user_tags
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150918053538) do
+ActiveRecord::Schema.define(version: 20150918055851) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -68,9 +68,11 @@ ActiveRecord::Schema.define(version: 20150918053538) do
     t.string   "commentable_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "products", force: true do |t|
     t.string   "title"
