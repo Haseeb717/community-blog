042f152b6ceb4207d78047405c52261466b5b9d7
phaedra <haseeb.ahmad717@gmail.com>
2015-09-18 20:00:09 +0500

    config mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Blog::Application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
-
+  config.action_mailer.default_url_options = { :host => 'http://mvp_beta.mashup.li/' }
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
