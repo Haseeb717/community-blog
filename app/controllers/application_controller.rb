049c97cfe1b2c9ef45f3f30dfc5baf038dd343a3
phaedra <haseeb.ahmad717@gmail.com>
2015-09-21 15:21:29 +0500

    sign up and login page changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  #layout :layout_by_resource
 
   protected
 
@@ -12,4 +13,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password) }
   end
+
+  # def layout_by_resource
+  #   if devise_controller? && resource_name == :user && action_name == 'new'
+  #     "devise"
+  #   else
+  #     "application"
+  #   end
+  # end
+
 end
---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,25 @@
+module DeviseHelper
+  def devise_error_messages!
+
+    flash_alerts = []
+
+    if !flash.empty?
+      flash_alerts.push(flash[:error]) if flash[:error]
+      flash_alerts.push(flash[:alert]) if flash[:alert]
+      flash_alerts.push(flash[:notice]) if flash[:notice]
+    end
+
+    return '' if resource.errors.empty?  && flash_alerts.empty?
+    errors = resource.errors.empty? ? flash_alerts : resource.errors.full_messages
+    #messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    messages = errors.map { |msg| content_tag(:li, " #{msg}") }.join
+    html = <<-HTML
+    <div class="alert alert-error alert-danger" id="devise-error"> <button type="button"
+    class="close" data-dismiss="alert">x</button>
+      #{messages}
+    </div>
+    HTML
+
+    html.html_safe
+  end
+end
