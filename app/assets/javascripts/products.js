03c34e8d4c672a638114b94a6b8eca18d3fc57dc
phaedra <haseeb.ahmad717@gmail.com>
2015-10-13 19:26:49 +0500

    multiple images deletion and product detail new layout

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,44 +1,24 @@
  $(document).ready(function(){
 
     jQuery(".best_in_place").best_in_place();
-
-//     // $(".like").click(function() {
-//     //     var id = $("#product_id").val();
-//     //     var url_link = '/products/'+id+'/product_likes_and_dislikes';
-//     //     var current_id = $(this).attr('id');
-//     //     $.ajax({
-//     //         url: url_link,
-//     //         type: "POST",
-//     //         data: {type: $(this).attr('class')},
-//     //         success: function (data) { 
-//     //             $("#"+id).removeClass(current_id).addClass('dislike'); 
-//     //         }
-//     //     });
-//     // });
-
     
-    // $(".product-page-detail .product-details-like ul li a").click(function() {
-    //     var id = $(this).attr('id');
-        
-    //     var current_id = $(this).attr('class');
-    //     $.ajax({
-    //         url: url_link,
-    //         type: "POST",
-    //         data: {type: current_id},
-    //         success: function (data) {
-    //             if(current_id = "like"){
-    //                 $("#"+id).removeClass(current_id).addClass('like'); 
-    //             }
-    //             else{
-    //                 $("#"+id).removeClass(current_id).addClass('dislike');
-    //             }
-                
-    //         }
-    //     });
-    // });
- 
-    
-    
+    $("#delete-img").click(function(e){
+      var checkValues = $('input[name=image_id]:checked').map(function(){
+          return $(this).val();
+      }).get();
+      console.log(checkValues);
+      var id = $("#product_id").val();
+      if(checkValues.length == 0) {
+          alert("Select any image to delete");
+      }
+      else{
+          $.ajax({
+              url: '/products/delete_product_images.js',
+              type: 'delete',
+              data: { ids: checkValues, id: id }
+          });
+      }  
+    });
 
 
     
