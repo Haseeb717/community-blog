3029d6fd3709ea89b1bfbca4635962aca3427d82
phaedra <haseeb.ahmad717@gmail.com>
2015-10-16 18:06:34 +0500

    layout changes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
   
+  $(document).ajaxSend(function(event, request, settings) {
+      $('#loading-indicator').show();
+    });
 
+    $(document).ajaxComplete(function(event, request, settings) {
+      $('#loading-indicator').hide();
+    });
+    
     
 });
